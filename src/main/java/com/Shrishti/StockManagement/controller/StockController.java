4fef45b2fe99711737d9d17beae262cda1d83f47
Anuhshka Srivastava <shrishtisrivastava19@gmail.com>
2023-05-19 15:07:40 +0530

    New Stock Commit

--- src/main/java/com/Shrishti/StockManagement/controller/StockController.java
+++ src/main/java/com/Shrishti/StockManagement/controller/StockController.java
@@ -5,6 +5,7 @@ import com.Shrishti.StockManagement.model.StockType;
 import com.Shrishti.StockManagement.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class StockController {
@@ -14,13 +15,51 @@ public class StockController {
 
     //Add New Stocks
     @PostMapping(value = "/stock")
-    private String addStock(@RequestBody Stock stock){
+    private String addStock(@RequestBody List<Stock> stock){
         return stockService.addStock(stock);
     }
 
     //Get Stocks Based On Type
     @GetMapping(value = "/stockType")
-    private Stock getStocksBasedOnType(@RequestParam StockType stockType){
+    private List<Stock> getStocksBasedOnType(@RequestParam StockType stockType){
         return stockService.getStocksBasedOnType(stockType);
+    }
+
+    //Get Stocks Above Price and Lower Date
+    @GetMapping(value = "/abovePrice/price/{price}/lowerDate/date/{date}")
+    private List<Stock> getStocksAbovePriceLowerDate(@PathVariable Double price , @PathVariable String date){
+        return stockService.getStocksAbovePriceLowerDate(price,date);
+    }
+
+    //Get All Stocks Above MarketCap
+    @GetMapping(value = "/cap/{capPercentage}")
+    private List<Stock> getStocksAboveMarketCap(@PathVariable Double capPercentage){
+        return stockService.getStocksAboveMarketCap(capPercentage);
+    }
+
+    //Update MarketCap By StockId
+    @PutMapping(value = "/cap/{capPercentage}/stockId/{stockId}")
+    private String updateMarketCapById(@PathVariable Double capPercentage, @PathVariable Integer stockId){
+        return stockService.updateMarketCapById(capPercentage, stockId);
+    }
+
+    //Update Type By StockId
+    @PutMapping(value = "/type/{type}/stockId/{stockId}")
+    private String updateTypeById(@PathVariable StockType type, @PathVariable Integer stockId){
+        return stockService.updateTypeById(type,stockId);
+    }
+
+    //Update Stocks By Id
+    @PutMapping(value = "/stock/{stockId}")
+    public String updateStockById(@RequestBody Stock myStock, @PathVariable Integer stockId)
+    {
+        return stockService.updateStockById(myStock, stockId);
+    }
+
+    //Delete Stocks where OwnerCount is Less Than or Equal to Given OwnerCount
+    @DeleteMapping(value = "/ownerCount/{count}")
+    public String removeStocksByOwnerCount(@PathVariable  Integer count)
+    {
+        return stockService.deleteStocksBasedOnCount(count);
     }
 }
--- src/main/java/com/Shrishti/StockManagement/service/StockService.java
+++ src/main/java/com/Shrishti/StockManagement/service/StockService.java
@@ -3,8 +3,12 @@ package com.Shrishti.StockManagement.service;
 import com.Shrishti.StockManagement.model.Stock;
 import com.Shrishti.StockManagement.model.StockType;
 import com.Shrishti.StockManagement.repository.IStockDao;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class StockService {
@@ -12,17 +16,52 @@ public class StockService {
     @Autowired
     IStockDao stockDao;
 
-    public String addStock(Stock stock) {
+    public String addStock(List<Stock> stock) {
 
-        if(!stockDao.existsById(stock.getStockId())){
-            stockDao.save(stock);
-            return stock.toString();
+        Iterable<Stock> stockList = stockDao.saveAll(stock);
+
+        if(stockList != null){
+            return "Added List Of Stocks!!!....";
         }
-        return "Stock Already Exists!!!";
+        return "could not added!!!...";
 
     }
 
-    public Stock getStocksBasedOnType(StockType stockType) {
-        return stockDao.getStocksBasedOnType(stockType);
+    public List<Stock> getStocksBasedOnType(StockType stockType) {
+        return stockDao.findByStockType(stockType);
+    }
+
+    public List<Stock> getStocksAbovePriceLowerDate(Double price, String date) {
+        LocalDateTime formatDate = LocalDateTime.parse(date);
+        return stockDao.findByStockPriceGreaterThanAndStockBirthDateLessThanOrderByStockName(price,formatDate);
+    }
+
+    public List<Stock> getStocksAboveMarketCap(Double capPercentage) {
+        return stockDao.getStocksAboveMarketCap(capPercentage);
+    }
+
+    @Transactional
+    public String updateMarketCapById(Double capPercentage, Integer stockId) {
+        stockDao.updateMarketCapById(capPercentage, stockId);
+        return "MarketCap Updated!!!";
+    }
+
+    @Transactional
+    public String updateTypeById(StockType type, Integer stockId) {
+        String enumValue = type.name();
+        stockDao.modifyStockTypeById(enumValue, stockId);
+        return "Stock Type Updated!!!";
+    }
+
+    @Transactional
+    public String updateStockById(Stock myStock, Integer stockId) {
+        stockDao.updateStockById(myStock.getStockName(),myStock.getStockPrice(),myStock.getStockBirthDate(),stockId);
+        return "Stock is Updated Successfully By Id!!!";
+    }
+
+    @Transactional
+    public String deleteStocksBasedOnCount(Integer count) {
+        stockDao.deleteStocksBasedOnCount(count);
+        return "Stock is Deleted based On Count!!!";
     }
 }
